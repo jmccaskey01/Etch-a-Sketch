ee4443f7c802464c936cd05bbda10916ad18b65c
Jordan McCaskey <jordan.mccaskey@gmail.com>
2019-06-06 15:43:33 -0400

    can now 'draw' on grid

--- etchasketch.js
+++ etchasketch.js
@@ -1,19 +1,27 @@
 const container = document.getElementById('container')
-//do container styling in styles.css
 
 
-let etchSquare;
+
+//add button that calls a function that does the thing below
+
+
 sideSize = prompt("Please enter the number of squares you would like each side to contain.");
 etchSquareSize = container.clientWidth/sideSize;
-console.log("etchSquareSize " + etchSquareSize);
-console.log(container.clientWidth);
-
 
 for (i=0; i < sideSize**2; i++) {
   etchSquare = document.createElement('div');
   etchSquare.className = "etchSquare";
   etchSquare.style.height = `${etchSquareSize}px`;
   etchSquare.style.width = `${etchSquareSize}px`;
-  etchSquare.style.lineHeight = `${etchSquareSize}px`;
+
+
+
+  etchSquare.addEventListener("mouseover", function(e) {
+      let i = 1;
+      e.currentTarget.className = "etchSquareDrawn";
+      console.log("I was moused over " + i + " times!");
+      i++;
+  });
+
   container.appendChild(etchSquare);
 }
