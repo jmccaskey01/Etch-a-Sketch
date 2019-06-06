7ee612854acb50d052d1b4b652ae45e2020840a5
Jordan McCaskey <jordan.mccaskey@gmail.com>
2019-06-06 16:19:43 -0400

    created reset button

--- etchasketch.js
+++ etchasketch.js
@@ -1,27 +1,35 @@
-const container = document.getElementById('container')
+const container = document.getElementById('container');
+const resetButton = document.getElementById('reset');
+
+function setupCanvas(){
+
+  sideSize = prompt("Please enter the number of squares you would like each side to contain.");
+
+  etchSquareSize = container.clientWidth/sideSize;
+
+  for (i=0; i < sideSize**2; i++) {
+    etchSquare = document.createElement('div');
+    etchSquare.className = "etchSquare";
+    etchSquare.style.height = `${etchSquareSize}px`;
+    etchSquare.style.width = `${etchSquareSize}px`;
 
 
 
-//add button that calls a function that does the thing below
-
-
-sideSize = prompt("Please enter the number of squares you would like each side to contain.");
-etchSquareSize = container.clientWidth/sideSize;
-
-for (i=0; i < sideSize**2; i++) {
-  etchSquare = document.createElement('div');
-  etchSquare.className = "etchSquare";
-  etchSquare.style.height = `${etchSquareSize}px`;
-  etchSquare.style.width = `${etchSquareSize}px`;
-
-
-
-  etchSquare.addEventListener("mouseover", function(e) {
-      let i = 1;
+    etchSquare.addEventListener("mouseover", function(e) {
       e.currentTarget.className = "etchSquareDrawn";
-      console.log("I was moused over " + i + " times!");
-      i++;
-  });
+    });
 
-  container.appendChild(etchSquare);
+    container.appendChild(etchSquare);
+  }
 }
+
+resetButton.addEventListener("click", function(){
+
+  for (i=container.childNodes.length; i > 0; i--) {
+    container.removeChild(container.childNodes[0]);
+  }
+  setupCanvas();
+});
+
+setupCanvas();
+//add button that calls a function that does the thing below
